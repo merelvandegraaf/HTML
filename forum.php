edb3ee81a1520bfc0611f0bf4987ccaeda12b7de
merelvandegraaf <45418344+merelvandegraaf@users.noreply.github.com>
2019-01-17 21:44:47 +0100

    forum en video's

---
+++ forum.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Forum</title>
+    <link rel="stylesheet" href="CSS/styles.css"/>
+    <meta name="description" content="Beroepsproduct website over voetbal">
+    <meta name="keywords" content="Voetbal">
+    <meta name="author" content="Brian van der Wiel & Merel van de Graaf">
+</head>
+<body>
+<?php
+include("php/header.php");
+?>
+<main>
+    Zoek op titel:<input type="search"><br>
+    <a id="berichtplaatsen" href="bericht_plaatsen.php">Maak een bericht aan</a>
+    <?php
+    include_once "php/config.php";
+    $sql= 'select * from posts';
+    $result = $connectie->query($sql);
+
+    while ($row = $result->fetch_assoc()){
+        ?>
+    <div class="forum">
+    <?php
+        echo "<h2>$row[kopje]</h2> 
+                <p>$row[bezoeker]</p>
+               <p>$row[tekst]</p>";}
+        ?>
+    </div>
+</main>
+<?php
+include("php/footer.php");
+?>
+</body>
+</html>
