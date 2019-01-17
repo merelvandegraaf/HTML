ea37960253b93a851caf4dc5d2595d671218aadb
merelvandegraaf <45418344+merelvandegraaf@users.noreply.github.com>
2019-01-17 15:51:07 +0100

    videos

---
+++ videos.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Video's</title>
+    <link rel="stylesheet" href="CSS/styles.css"/>
+    <meta name="description" content="Beroepsproduct website over voetbal">
+    <meta name="keywords" content="Voetbal">
+    <meta name="author" content="Brian van der Wiel & Merel van de Graaf">
+</head>
+<body>
+<?php
+include("php/header.php");
+?>
+<main>
+    Zoek op titel:<input type="search">
+    <iframe name="film" width="560" height="315" src="https://www.youtube.com/embed/dnN_UQUwAvU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="flex-horizontal">
+        <div>
+            <h2>WK 2014 terugblik Nederland</h2>
+            <p>Terug blik van een heel mooi wk voor Nederland.</p>
+            <a href="https://www.youtube.com/embed/dnN_UQUwAvU" target="film"><img src="Afbeeldingen/Video1.PNG" alt="video1" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>Marokko - Iran (groep B) I WK 2018</h2>
+            <p>De samenvatting van de wedstrijd Marokko - Iran op het WK 2018.</p>
+            <a href="https://www.youtube.com/embed/0SktBrRziHU" target="film"><img src="Afbeeldingen/Video2.PNG" alt="video2" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>Portugal - Spanje (groep B) I WK 2018</h2>
+            <p>De samenvatting van de wedstrijd Portugal - Spanje op het WK 2018</p>
+            <a href="https://www.youtube.com/embed/hvYEZcbrBYo" target="film"><img src="Afbeeldingen/Video3.PNG" alt="video3" style="border:0;"></a>
+        </div>
+    </div>
+    <div class="flex-horizontal">
+        <div>
+            <h2>World Cup 2010 • The Netherlands • Our Story</h2>
+            <p>Dit is een film over het geweldige WK van 2010.</p>
+            <a href="https://www.youtube.com/embed/uyIT60HKmU8" target="film"><img src="Afbeeldingen/Video4.PNG" alt="video4" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>Highlights Nederland - Zweden (10/10/2017)</h2>
+            <p>Oranje won het laatste WK-kwalificatieduel met 2-0 van Zweden, maar gaat definitief niet naar het WK</p>
+            <a href="https://www.youtube.com/embed/n9jNP0-uFNQ" target="film"><img src="Afbeeldingen/Video5.PNG" alt="video5" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>Belgium v Japan - 2018 FIFA World Cup Russia</h2>
+            <p>Belgium battle back from 2 goals down to win 3-2 with the final kick of the game, setting up a quarter final with Brazil</p>
+            <a href="https://www.youtube.com/embed/fJeJuc27ggE" target="film"><img src="Afbeeldingen/Video6.PNG" alt="video6" style="border:0;"></a>
+        </div>
+    </div>
+    <div class="flex-horizontal">
+        <div>
+            <h2>Frankrijk - Argentinië (achtste finale) | WK2018</h2>
+            <p>De samenvatting van Frankrijk - Argentinië tijdens het WK in Rusland.</p>
+            <a href="https://www.youtube.com/embed/MMP0HzwK-D8" target="film"><img src="Afbeeldingen/Video7.PNG" alt="video7" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>HIGHLIGHTS | Duitsland - Spanje</h2>
+            <p>De samenvatting van Duitsland - Spanje, een vriendschappelijke wedstrijd gespeeld op vrijdag 23 maart 2018. </p>
+            <a href="https://www.youtube.com/embed/dnN_UQUwAvU" target="film"><img src="Afbeeldingen/Video8.PNG" alt="video8" style="border:0;"></a>
+        </div>
+        <div>
+            <h2>Brazilië - België (kwartfinale) I WK 2018</h2>
+            <p>Samenvatting van de kwartfinale tussen Brazilië en België op het WK in Rusland.</p>
+            <a href="https://www.youtube.com/embed/ZuYY85_Nkbc" target="film"><img src="Afbeeldingen/Video9.PNG" alt="video9" style="border:0;"></a>
+        </div>
+    </div>
+</main>
+<?php
+include("php/footer.php");
+?>
+</body>
+</html>
